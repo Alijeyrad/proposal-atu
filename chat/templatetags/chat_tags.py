ed7f7adc4335854cbe786b71740416ca6ae84a94
Ali Julaee Rad <alijeyrad@gmail.com>
2023-09-01 17:50:03 +0330

    unread message notification

---
+++ chat/templatetags/chat_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.db.models import Q
+from chat.models import Message
+
+register = template.Library()
+
+@register.inclusion_tag('messages/templatetags/message_notif.html')
+def show_notif(user):
+    unread_count = Message.objects.filter(
+        Q(receiver = user) & Q(is_read = False)
+    ).count()
+
+    should_show = False if unread_count == 0 else True
+
+    return {'should_show': should_show}
